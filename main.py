46711a6058a017a1b44f27d249297854b6e570ed
Evgenion47 <62439770+Evgenion47@users.noreply.github.com>
2021-12-22 18:34:35 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,108 +1,65 @@
 from scipy.optimize import minimize
+from tabulate import tabulate
 from math import exp
 
-# функция, которую минимизируем
-#fx = lambda x: pow(x[0], 2) - 4 * x[0]
+def sciM():
+    g = [lambda x: -(-x[0] - x[1] + 2), lambda x: -(x[0] - 2 * x[1] + 1), lambda x: -(-2 * x[0] + x[1])]
+    x0 = [0.8, 1.3]
+    cons = ({'type': 'ineq', 'fun': g[0]},
+            {'type': 'ineq', 'fun': g[1]},
+            {'type': 'ineq', 'fun': g[2]})
+    answer = minimize(fx, x0, constraints=cons)
+    print('scipy:', answer.x)
 
-fx = lambda x: 2 * pow(x[0], 2) + pow(x[1], 2)
-
-#fx = lambda x: pow((1 - x[0]), 2) + 100 * pow((x[1] - pow(x[0], 2)), 2)
-
-# условия
-#g1 = lambda x: -(x[0] - 1)
-
-
-g1 = lambda x: -(-x[0] - x[1] + 2)
-g2 = lambda x: -(x[0] - 2 * x[1] + 1)
-g3 = lambda x: -(-2 * x[0] + x[1])
-
-
-# g1 = lambda x: -(x[0] - 2 * x[1] + 2)
-# g2 = lambda x: -(-x[0] - 2 * x[1] + 6)
-# g3 = lambda x: -(-x[0] + 2 * x[1] + 2)
-
-g = [g1, g2, g3]
-#g=[g1]
-
-# начальная точка
-x0 = [0.8, 1.3]
-
-# используя scipy.optimize
-cons = ({'type': 'ineq', 'fun': g1},
-        {'type': 'ineq', 'fun': g2},
-        {'type': 'ineq', 'fun': g3})
-# cons = ({'type': 'ineq', 'fun': g1})
-answer = minimize(fx, x0, constraints=cons)
-print('scipy:', answer.x)
-
-# g1 = lambda x: (x[0] - 1)
-# g=[g1]
-
-# g1 = lambda x: (x[0] - 2 * x[1] + 2)
-# g2 = lambda x: (-x[0] - 2 * x[1] + 6)
-# g3 = lambda x: (-x[0] + 2 * x[1] + 2)
-
-g1 = lambda x: (-x[0] - x[1] + 2)
-g2 = lambda x: (x[0] - 2 * x[1] + 1)
-g3 = lambda x: (-2 * x[0] + x[1])
-
-# Знаки в условиях между scipy и кодом ниже должны отличаться
-
-g = [g1, g2, g3]
-
-# мой вариант метода штрафной функции
-x0 = [0.8, 1.3]
-eps = 0.0001
-C = 1
-beta = 2
-#pen4 0 < beta < 1
-#pen1-2 beta > 1
-k = 1
-pen = 10
-INF = 10 ** 10
-
-
-
-def getPen4(x):
-    lessZero = True
-    for i in g:
-        lessZero = lessZero and i(x) < 0
-    if lessZero:
-        return (-1) * C * sum([1 / i(x) for i in g])
-    else:
-        return INF
-
-
-def getPen1(x):
+def getPen1(x, C):
     q = 2
     return C * sum([pow(max(0, i(x)), q) for i in g])
 
-
-def getPen2(x):
-    lessOrEqZero = True
+def getPen2(x, C):
+    negorzr = True
     for i in g:
-        lessOrEqZero = lessOrEqZero and i(x) <= 0
-    if lessOrEqZero:
+        negorzr = negorzr and i(x) <= 0
+    if negorzr:
         return 0
     else:
         return C * exp((-1) / max([i(x) for i in g]))
 
+def getPen4(x, C):
+    neg = True
+    for i in g:
+        neg = neg and i(x) < 0
+    if neg:
+        return (-1) * C * sum([1 / i(x) for i in g])
+    else:
+        return pow(10, 10)
 
-while pen > eps:
-    Fx = lambda x: fx(x) + getPen2(x)
-    x0 = minimize(Fx, x0).x
-    pen = getPen2(x0)
-    print('k = ', k, 'x = ', x0, 'pen = ', pen, 'C = ', C)
-    C *= beta
-    k += 1
-    if (k >= 1000):
-        print('Многовато, блин')
-        break
+fx = lambda x: 2 * pow(x[0], 2) + pow(x[1], 2)
 
-print('Моя реализация:', x0)
+g = [lambda x: (-x[0] - x[1] + 2), lambda x: (x[0] - 2 * x[1] + 1), lambda x: (-2 * x[0] + x[1])]
 
+def MPF(penalty, eps, x0, C, b, pv):
+    k = 1
+    table = [['k', 'x', 'penalty', 'C'], [0, x0, penalty, C]]
+    while penalty > eps:
+        Fx = {
+            1: lambda x: fx(x) + getPen1(x, C),
+            2: lambda x: fx(x) + getPen2(x, C),
+            4: lambda x: fx(x) + getPen4(x, C)
+        }.get(pv)
+        x0 = minimize(Fx, x0).x
+        penalty = {
+            1: getPen1(x0, C),
+            2: getPen2(x0, C),
+            4: getPen4(x0, C)
+        }.get(pv)
+        table.append([k, x0, penalty, C])
+        C *= b
+        k += 1
+        if (k >= 1000):
+            break
+    print(tabulate(table, tablefmt="fancy_grid") + '\n')
 
-# todo Организовать функцию, в которую будем передавать саму функцию минимизации, условия, eps, и выбор метода
-# todo организовать вывод таблицы
-# todo пожанглировать кодом
-# todo попитониться
+#sciM()
+MPF(10, 0.0001, [0.8, 1.3], 1, 2, 1)
+MPF(10, 0.0001, [0.8, 1.3], 1, 2, 2)
+MPF(10, 0.0001, [0.8, 1.3], 1, 0.5, 4)
